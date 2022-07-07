aa0b889319f26120fa60ffd77fd0eaa6feb51e35
Nils Vu <owls@nilsvu.de>
2022-07-07 17:29:49 +0200

    Rich progress bar for download_data

--- gwpv/download_data.py
+++ gwpv/download_data.py
@@ -2,39 +2,56 @@ import logging
 import os
 
 import requests
-import tqdm
+import rich.progress
 
 from gwpv.scene_configuration import parse_as
 
+logger = logging.getLogger(__name__)
+progress = rich.progress.Progress(
+    "  ",
+    rich.progress.BarColumn(),
+    rich.progress.DownloadColumn(),
+    rich.progress.TransferSpeedColumn(),
+    rich.progress.TimeRemainingColumn(),
+)
+
+
+def _download_file(url, filename, task_id):
+    url_response = requests.get(url, stream=True)
+    file_size = int(url_response.headers.get("content-length", 0))
+    filename_tmp = filename + "_tmp"
+    progress.update(task_id, total=file_size)
+    with open(filename_tmp, "wb") as cached_file, progress:
+        progress.start_task(task_id)
+        for data in url_response.iter_content(chunk_size=1024):
+            progress.update(task_id, advance=cached_file.write(data))
+    os.rename(filename_tmp, filename)
+
 
 def download_data(datasources):
-    logger = logging.getLogger(__name__)
-    for datasource_name, datasource_config in datasources.items():
-        try:
-            url, cached_filename = parse_as.remote_file(datasource_config)
-        except ValueError:
-            continue
-        if os.path.exists(cached_filename):
-            logger.debug(
-                f"Found cached {datasource_name} file '{cached_filename}' for"
-                f" URL '{url}'"
+    with progress:
+        for datasource_name, datasource_config in datasources.items():
+            try:
+                url, cached_filename = parse_as.remote_file(datasource_config)
+            except ValueError:
+                continue
+            if os.path.exists(cached_filename):
+                logger.debug(
+                    f"Found cached {datasource_name} file '{cached_filename}'"
+                    f" for URL '{url}'"
+                )
+                continue
+            logger.info(
+                f"Downloading {datasource_name} file at URL '{url}' to cache"
+                f" '{cached_filename}'..."
             )
-            continue
-        logger.info(
-            f"Downloading {datasource_name} file at URL '{url}' to cache"
-            f" '{cached_filename}'..."
-        )
-        os.makedirs(os.path.dirname(cached_filename), exist_ok=True)
-        url_response = requests.get(url, stream=True)
-        file_size = int(url_response.headers.get("content-length", 0))
-        cached_filename_tmp = cached_filename + "_tmp"
-        with open(cached_filename_tmp, "wb") as cached_file, tqdm.tqdm(
-            desc="Downloading " + datasource_name,
-            total=file_size,
-            unit="iB",
-            unit_scale=True,
-            unit_divisor=1024,
-        ) as progress:
-            for data in url_response.iter_content(chunk_size=1024):
-                progress.update(cached_file.write(data))
-        os.rename(cached_filename_tmp, cached_filename)
+            os.makedirs(os.path.dirname(cached_filename), exist_ok=True)
+            progress.console.print(
+                f"Downloading [bold]{datasource_name}[/bold]"
+            )
+            task_id = progress.add_task(
+                "download",
+                start=False,
+                datasource_name=datasource_name,
+            )
+            _download_file(url=url, filename=cached_filename, task_id=task_id)
