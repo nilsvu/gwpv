6052261a9bf58a50589007928fed9b7d9646331d
Nils Vu <owls@nilsvu.de>
2022-07-20 22:23:43 +0200

    Show rich progress during movie rendering

--- gwpv/render/movie.py
+++ gwpv/render/movie.py
@@ -1,28 +1,40 @@
 import os
 import subprocess
 
+import rich.progress
+
 
 def render_movie(output_filename, frames_dir, frame_rate):
-    subprocess.call(
-        [
-            "ffmpeg",
-            "-vcodec",
-            "png",
-            "-framerate",
-            str(frame_rate),
-            "-i",
-            os.path.join(frames_dir, r"frame.%06d.png"),
-            "-pix_fmt",
-            "yuv420p",
-            "-vcodec",
-            "libx264",
-            "-crf",
-            "17",
-            "-threads",
-            "0",
-            "-preset",
-            "slow",
-            "-y",
-            output_filename + ".mp4",
-        ]
-    )
+    with rich.progress.Progress(
+        rich.progress.TextColumn("[progress.description]{task.description}"),
+        rich.progress.SpinnerColumn(
+            spinner_name="simpleDots", finished_text="... done."
+        ),
+    ) as progress:
+        task_id = progress.add_task("Rendering movie", total=1)
+        proc = subprocess.run(
+            [
+                "ffmpeg",
+                "-vcodec",
+                "png",
+                "-framerate",
+                str(frame_rate),
+                "-i",
+                os.path.join(frames_dir, r"frame.%06d.png"),
+                "-pix_fmt",
+                "yuv420p",
+                "-vcodec",
+                "libx264",
+                "-crf",
+                "17",
+                "-threads",
+                "0",
+                "-preset",
+                "slow",
+                "-y",
+                output_filename + ".mp4",
+            ],
+            capture_output=True,
+            check=True,
+        )
+        progress.update(task_id, completed=1)
